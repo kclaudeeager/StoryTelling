7502f53a832ff9a083a4be7e1f6c86b62625880e
kclaudeeager <claudekwizera003@gmail.com>
2024-02-18 18:10:01 +0200

    Getting stories

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from starlette.templating import Jinja2Templates
 import asyncpg
 from pydantic import BaseModel
 import os
+import uuid
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -48,15 +49,25 @@ async def shutdown():
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.post("/story")
 async def add_story(story: Story):
+    story_id = str(uuid.uuid4())
     await app.state.db.execute('''
         INSERT INTO stories VALUES ($1, $2, $3, $4, $5, $6)
-    ''', story.story_id, story.story_text, story.genre, story.origin, story.demographic, story.themes)
-    return {"message": "Story added successfully"}
+    ''', story_id, story.story_text, story.genre, story.origin, story.demographic, story.themes)
+    return {"message": "Story added successfully", "story_id": story_id}
 
-@app.get("/stories")
-async def get_stories():
+@app.get("/stories-show", response_class=HTMLResponse)
+async def get_stories(request: Request):
     rows = await app.state.db.fetch("SELECT * FROM stories")
     stories = [dict(row) for row in rows]
-    return {"stories": stories}
+    # render the stories in the template using the storeis.html file
+    return templates.TemplateResponse("stories.html", {"request": request, "stories": stories})
+
+@app.get("/stories")
+async def get_stories(request: Request):
+    rows = await app.state.db.fetch("SELECT * FROM stories")
+    stories = [dict(row) for row in rows]
+    
+    return stories
