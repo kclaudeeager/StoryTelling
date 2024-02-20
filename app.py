a2b2a245492f8ede24e40b72342e34c6dab46b21
kclaudeeager <claudekwizera003@gmail.com>
2024-02-20 17:38:14 +0200

    Uing dictionary for demographics

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
-from fastapi import FastAPI, Request,HTTPException
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse 
 from starlette.templating import Jinja2Templates
 import asyncpg
 from pydantic import BaseModel
 import os
 import uuid
-from typing import List
 from dotenv import load_dotenv
+
 load_dotenv()
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-app = FastAPI()
 class Story(BaseModel):
     story_id: str
     story_title: str
     story_text: str
     genre: str
     size: str
-    demographic: List[str] = []  # Default value as an empty list
+    demographic: dict
     themes: str
 
 async def get_db():
-   
     conn = await asyncpg.connect(
         user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD"),
@@ -43,11 +41,11 @@ async def startup():
             story_text TEXT,
             genre TEXT,
             size TEXT,
-            demographic TEXT[],
+            demographic JSONB,
             themes TEXT
         )
     ''')
-    
+
 @app.on_event("shutdown")
 async def shutdown():
     await app.state.db.close()
@@ -61,8 +59,8 @@ async def read_root(request: Request):
 async def add_story(story: Story):
     story_id = str(uuid.uuid4())
     await app.state.db.execute('''
-        INSERT INTO stories VALUES ($1, $2, $3, $4, $5, $6)
-    ''', story_id, story.story_text, story.genre, story.size, story.demographic, story.themes)
+        INSERT INTO stories VALUES ($1, $2, $3, $4, $5, $6, $7)
+    ''', story_id, story.story_title, story.story_text, story.genre, story.size, story.demographic, story.themes)
     return {"message": "Story added successfully", "story_id": story_id}
 
 @app.get("/stories-show", response_class=HTMLResponse)
@@ -96,19 +94,20 @@ async def delete_story(story_id: str):
     if result == "DELETE 0":
         raise HTTPException(status_code=404, detail="Story not found")
     return {"message": "Story deleted successfully"}
+
 # delete all and reset the database
 @app.delete("/stories")
 async def delete_stories():
     result = await app.state.db.execute("DELETE FROM stories")
     return {"message": "All stories deleted successfully"}
+
 # Update a story by its ID
 @app.put("/story/{story_id}")
 async def update_story(story_id: str, story: Story):
     # Validate input data here if necessary
     result = await app.state.db.execute('''
-        UPDATE stories SET story_title = $1, story_text = $2, genre = $3, origin = $4, demographic = $5, themes = $6 WHERE story_id = $7
-    ''',story.story_title, story.story_text, story.genre, story.size, story.demographic, story.themes, story_id)
+        UPDATE stories SET story_title = $1, story_text = $2, genre = $3, size = $4, demographic = $5, themes = $6 WHERE story_id = $7
+    ''', story.story_title, story.story_text, story.genre, story.size, story.demographic, story.themes, story_id)
     if result == "UPDATE 0":
         raise HTTPException(status_code=404, detail="Story not found")
     return {"message": "Story updated successfully"}
-
